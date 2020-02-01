7a985b6cde5f776fcfd2fc70dd0713c115ad1c2f
Mathew Vaughan <vaughanmathew@hotmail.com>
2020-02-01 23:44:02 +0000

    remove package with network dependency from tests

--- documents/conftest.py
+++ documents/conftest.py
@@ -1,12 +1,24 @@
 import pytest
 from rest_framework.test import APIClient
 from typing import List
-
-from random_word import RandomWords
-r = RandomWords()
+import itertools
+from string import ascii_uppercase as letters, digits
 
 DOCUMENT_ENDPOINT = "/api/document/"
 SEARCH_ENDPOINT = "/search/"
+
+
+def generate_word(words=itertools.product(letters, letters, digits, digits, digits)):
+	for word in words:
+		yield "".join(word)
+
+
+@pytest.fixture
+def random_words():
+	def __random_words():
+		return [next(generate_word()) for _ in range(10)]
+
+	return __random_words
 
 
 @pytest.fixture
@@ -16,29 +28,46 @@ def api_client():
 
 
 @pytest.fixture
-def create_document(api_client: APIClient, create_content):
+def post_document(api_client: APIClient, create_content_from_sentences):
 	"""Returns a helper function that creates documents with specified content"""
+
 	def __create_document(content: str = None, sentences: List[str] = None, title: str = "test_doc.txt"):
 		"""Use DRF test client to create a document via the API"""
 		if sentences:
-			content = create_content(sentences)
+			content = create_content_from_sentences(sentences)
 		return api_client.post(DOCUMENT_ENDPOINT, {"title": title, "content": content}, format="json")
+
 	return __create_document
 
 
 @pytest.fixture
-def create_sentences():
-	"""Returns a helper function that creates a sentence list with specified number of sentences"""
+def create_sentences(random_words):
+	"""Returns a helper function that returns a list of sentences with specific length"""
+
 	def __create_sentences(sentence_count: int):
 		"""Generate and return random sentences"""
-		return [" ".join(r.get_random_words(limit=10)) for i in range(sentence_count - 1)]
+		return [" ".join(random_words()) for i in range(sentence_count - 1)]
+
 	return __create_sentences
 
 
 @pytest.fixture
-def create_content():
+def create_content(create_sentences):
 	"""Returns a helper function that creates document content with specified number of sentences"""
-	def __create_content(sentences: List[str] = None):
+
+	def __create_content(sentence_count: int):
 		"""Generate and return random content"""
+		return ". ".join(create_sentences(sentence_count)) + "."
+
+	return __create_content
+
+
+@pytest.fixture
+def create_content_from_sentences():
+	"""Returns a helper function that creates document content with specified sentences"""
+
+	def __create_content(sentences: List[str] = None):
+		"""Join sentences into content"""
 		return ". ".join(sentences) + "."
+
 	return __create_content
--- documents/tests.py
+++ documents/tests.py
@@ -1,12 +1,11 @@
 import pytest
-from .conftest import DOCUMENT_ENDPOINT, SEARCH_ENDPOINT
-
-
+from .conftest import SEARCH_ENDPOINT
+from .models import Document
 
 
 @pytest.mark.django_db
-def test_document_creation(create_document):
-    result = create_document(
+def test_document_creation(post_document):
+    result = post_document(
         title="test_document.txt",
         content="Test body"
     )
@@ -16,11 +15,10 @@ def test_document_creation(create_document):
 
 
 @pytest.mark.django_db
-def test_document_saving_returns_sentences(create_document, create_sentences):
+def test_document_saving_returns_sentences(post_document, create_sentences):
     number_of_sentences = 3
     sentences = create_sentences(sentence_count=number_of_sentences)
-    result = create_document(sentences=sentences)
-
+    result = post_document(sentences=sentences)
     # TODO: I will be removing sentences from the response. This was partly a spike and
     # TODO: was here to drive out test suite capability, and also explore the simplest way to add
     # TODO: the concept of sentences into my application
